0a0cf7b889f0c6cd495c1ae2138074a79610f238
Daniil Mironenko <dan.mironenko@gmail.com>
2024-02-22 16:10:34 +0300

    style users table schema

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS delivery_schema.users(
     user_id text PRIMARY KEY DEFAULT uuid_generate_v4(),
     username varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
-    password_hash varchar(255) NOT NULL,
     user_type delivery_schema.user_type NOT NULL DEFAULT 'regular',
+    password_hash varchar(255) NOT NULL,
     CONSTRAINT uniq_username UNIQUE (username),
     CONSTRAINT uniq_email UNIQUE (email)
 );
