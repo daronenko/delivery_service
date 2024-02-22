94b9ce146d2a5e0400aa30705a5070faa12c86dc
Daniil Mironenko <dan.mironenko@gmail.com>
2024-02-22 12:52:45 +0300

    add users table to db schema

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -1,8 +1,17 @@
-DROP SCHEMA IF EXISTS hello_schema CASCADE;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE SCHEMA IF NOT EXISTS hello_schema;
+DROP SCHEMA IF EXISTS delivery_schema CASCADE;
 
-CREATE TABLE IF NOT EXISTS hello_schema.users (
-    name TEXT PRIMARY KEY,
-    count INTEGER DEFAULT(1)
+CREATE SCHEMA IF NOT EXISTS delivery_schema;
+
+CREATE TYPE delivery_schema.user_type AS ENUM ('regular', 'courier');
+
+CREATE TABLE IF NOT EXISTS delivery_schema.users(
+    user_id text PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password_hash varchar(255) NOT NULL,
+    user_type delivery_schema.user_type NOT NULL DEFAULT 'regular',
+    CONSTRAINT uniq_username UNIQUE (username),
+    CONSTRAINT uniq_email UNIQUE (email)
 );
