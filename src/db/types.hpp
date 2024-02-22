61ce1e8d7948fc1de482cc9091bb14a31f638df1
Daniil Mironenko <dan.mironenko@gmail.com>
2024-02-22 16:17:16 +0300

    add variable with user type

---
+++ src/db/types.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string_view>
+
+
+namespace delivery_service::db::types {
+
+inline constexpr std::string_view kUser{"delivery_schema.user"};
+
+}  // namespace delivery_service::db::types
