f87c82d574264aac043613c2d6e9d5bf19a0de48
Daniil Mironenko <dan.mironenko@gmail.com>
2024-02-23 21:16:09 +0300

    add dto for user

---
+++ src/dto/user.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+
+#include <userver/formats/json.hpp>
+#include <userver/formats/parse/common_containers.hpp>
+
+
+namespace delivery_service::dto {
+
+struct UserRegistrationDTO {
+  std::optional<std::string> username;
+  std::optional<std::string> email;
+  std::optional<std::string> password;
+  std::optional<std::string> user_type;
+};
+
+struct UserLoginDTO {
+  std::optional<std::string> email;
+  std::optional<std::string> password;
+};
+
+struct UserUpdateDTO {
+  std::optional<std::string> email;
+  std::optional<std::string> username;
+  std::optional<std::string> password;
+  std::optional<std::string> user_type;
+};
+
+UserRegistrationDTO Parse(const userver::formats::json::Value& json,
+                          userver::formats::parse::To<UserRegistrationDTO>);
+
+UserLoginDTO Parse(const userver::formats::json::Value& json,
+                   userver::formats::parse::To<UserLoginDTO>);
+
+UserUpdateDTO Parse(const userver::formats::json::Value& json,
+                    userver::formats::parse::To<UserUpdateDTO>);
+
+}  // namespace delivery_service::dto
